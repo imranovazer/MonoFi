dbbe7c57bdccd56b4ad6826a4d926c9b034c3472
Azer <imranovazer@gmail.com>
2023-02-08 20:39:38 +0400

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import "../styles/Home.scss";
+import Navbar from "../components/Navbar";
+const Home = () => {
+  return (
+    <div className="Home">
+      <div className="container forHome"></div>
+    </div>
+  );
+};
+
+export default Home;
